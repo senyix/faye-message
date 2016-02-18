58cc45e4564fcf535a54eea8dffb610c8dd3467e
Antonio Antillon <antillas21@gmail.com>
2016-02-17 20:41:52 -0800

    Adds task to start|stop faye server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 lock "3.4.0"
 
 set :application, "faye_message_server"
+set :user, "deploy"
 set :repo_url, "git@github.com:resuelve-tu-deuda/faye-message-server.git"
 
 # Default branch is :master
@@ -9,6 +10,9 @@ set :repo_url, "git@github.com:resuelve-tu-deuda/faye-message-server.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/apps/#{fetch(:application)}"
+
+set :faye_pid, "#{deploy_to}/shared/tmp/pids/faye.pid"
+set :faye_config, "#{deploy_to}/current/config.ru"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -28,7 +32,13 @@ set :ssh_options, { forward_agent: true }
 set :linked_files, fetch(:linked_files, []).push("config/faye.yml")
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+set :linked_dirs, fetch(:linked_dirs, []).
+  push(
+    "log",
+    "tmp/pids",
+    "tmp/cache",
+    "tmp/sockets"
+  )
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -66,5 +76,25 @@ namespace :deploy do
       # end
     end
   end
-
 end
+
+namespace :faye do
+  desc "Start Faye"
+  task :start do
+    on roles(:app) do
+      within(release_path) do
+        execute :rbenv, :sudo, "bundle exec puma #{fetch(:faye_config)} -e production -d --pidfile #{fetch(:faye_pid)}"
+      end
+    end
+  end
+
+  desc "Stop Faye"
+  task :stop do
+    on roles(:app) do
+      execute :sudo, :kill, "`cat #{fetch(:faye_pid)}` || true"
+    end
+  end
+end
+
+before 'deploy:updated', 'faye:stop'
+after 'deploy:finished', 'faye:start'
