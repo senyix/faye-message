a6028504569c42178e0f70218b0232c3b316d979
Antonio Antillon <antillas21@gmail.com>
2016-02-17 18:46:12 -0800

    Initial capistrano config settings for deployment

--- Gemfile
+++ Gemfile
@@ -9,4 +9,15 @@ gem "puma"
 
 group :development do
   gem "pry"
+
+  # Deployment section
+  gem 'capistrano', '~> 3.4.0'
+  gem 'capistrano-bundler', '~> 1.1.4'
+  # Add this if you're using rbenv (in the production server)
+  gem 'capistrano-rbenv', github: "capistrano/rbenv"
+  # Add this if you're using rvm (in the production server)
+  # gem 'capistrano-rvm'
+
+  # Other capistrano goodies
+  gem 'capistrano-safe-deploy-to', '~> 1.1.1'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,70 @@
+# config valid only for current version of Capistrano
+lock "3.4.0"
+
+set :application, "faye_message_server"
+set :repo_url, "git@github.com:resuelve-tu-deuda/faye-message-server.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/apps/#{fetch(:application)}"
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+set :ssh_options, { forward_agent: true }
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push("config/faye.yml")
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  desc "Copy files"
+  task :copy do
+    on roles(:app) do |host|
+       %w[ faye.yml ].each do |file|
+          upload! "config/#{file}", "#{shared_path}/config/#{file}"
+       end
+    end
+  end
+
+  desc "Remove dotfiles"
+  task :remove_dotfiles do
+    on roles(:app) do |host|
+      %w(.ruby-version).each do |dot_file|
+        execute :rm, "#{release_path}/#{dot_file}"
+      end
+    end
+  end
+
+  before "check:linked_files", :copy
+  after :updated, :remove_dotfiles
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, "cache:clear"
+      # end
+    end
+  end
+
+end
